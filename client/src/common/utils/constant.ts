6e683cf651d68870673a1528922fe086f0a8b141
sonthanhngo <ngothanhson123456@gmail.com>
2024-04-22 12:08:08 +0700

    refactor

--- client/src/common/utils/constant.ts
+++ client/src/common/utils/constant.ts
@@ -1,9 +1,11 @@
 export const CONTRACT_ADDRESS = '0x3335FD6a4aaab9160B5CD5f8778F8193f9aBeAB2';
 
-export const ETH_PRICE: number = await (async () => {
-  const response = await fetch(
-    'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
-  );
-  const data = await response.json();
-  return data.USD;
-})();
+// export const ETH_PRICE: number = await (async () => {
+//   const response = await fetch(
+//     'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
+//   );
+//   const data = await response.json();
+//   return data.USD;
+// })();
+
+export const ETH_PRICE: number = 3197;
